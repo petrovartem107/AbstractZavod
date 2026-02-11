b7d91fffdd4bb82a1b02eaf8afe5ade9c4f63215
petrovartem107 <petrovartem353@gmail.com>
2026-02-11 16:09:12 +0300

    Добавиk 7, 13, 25-29 строки. ну он не щазодит в интерфейс(в интерфейсе есть реализация) ну я и вызываю через фабрику конечно. Ну я значит не знаю как)

--- AbstractZavod/Program.cs
+++ AbstractZavod/Program.cs
@@ -4,24 +4,28 @@ ChainikovZavod chainikovZavod = new ChainikovZavod();
 HolodilnikovZavod holodilnikovZavod = new HolodilnikovZavod();
 Kavaz kavaz = new Kavaz();
 Fabrica fabrica = new Fabrica();
+IPredpiyatie zavod = new Fabrica();
 
 kavaz.Proizvodit();
 holodilnikovZavod.Proizvodit();
 chainikovZavod.Proizvodit();
 fabrica.Realization();
+Zapusk(zavod);
 
 IPredpiyatie kavazPredpriyatie = kavaz;
 IPredpiyatie holodilnikovPredpriyatie = holodilnikovZavod;
 IPredpiyatie chainikovPredpriyatie = chainikovZavod;
 
+
+
 kavazPredpriyatie.Realization();
 holodilnikovPredpriyatie.Realization();
 chainikovPredpriyatie.Realization();
 
-static void Zapusk(IPredpiyatie ipred)
-{
-    ipred.Realization();
-}
+ static void Zapusk(IPredpiyatie ipred)
+ {
+     ipred.Realization();
+ }
 
 
 Console.ReadLine();
